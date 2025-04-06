f196038b9c5b07a2ebb68ca7a12f35d993418506
yuliana669 <yulianacarrilloaza@gmail.com>
2025-04-06 18:08:32 -0500

    Connecting DB - HTTP:GET

--- src/controllers/categoria.controllers.js
+++ src/controllers/categoria.controllers.js
@@ -1,8 +1,18 @@
-const getCategorias = (req, res) => {
-
-    res.json({"categoria":"Electrodomesticos"})
-
+import getConnection from "./../db/database.js"
+const getCategorias = async (req, res) => {
+    try {
+        const connection = await getConnection();
+        const result = await connection.query("SELECT CategoriaID, CategoriaNombre,Descripcion,Imagen FROM categorias")
+    
+        res.json(result);
+    
+    } catch (error) {
+        console.error("ERROR 500", error);
+        res.status(500).json({ message: "Error al obtener categorías", error });
+    }
+   
 } 
+
 
 export const methodHTTP = {
     getCategorias 
